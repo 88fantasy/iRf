8d50dd63bbddd88b8d69498876af609ec93fd577
pro <xianweijian@MacBook-Pro.local>
2011-07-19 09:49:34 +0800

    init

---
+++ iRf/Const.h
@@ -0,0 +1,13 @@
+/*
+ *  Const.h
+ *  rf
+ *
+ *  Created by pro on 11-7-8.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+static NSString *base = @"http://www.gzmpc.com:8060/gzmpcscm3";
+static NSString *host = @"www.gzmpc.com";
